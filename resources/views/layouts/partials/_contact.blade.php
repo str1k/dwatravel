c3ed0bd4a593084609c243683036f5a67f24429b
Strikermx <y.thanasit@gmail.com>
2017-02-25 02:47:07 -0800

    fix responsive contact bar

--- resources/views/layouts/partials/_contact.blade.php
+++ resources/views/layouts/partials/_contact.blade.php
@@ -7,8 +7,7 @@
               <li><a><span class="glyphicon glyphicon-earphone"></span> 0909686520</a></li>
               <li><a><span class="glyphicon glyphicon-envelope"></span> y.thanasit@gmail.com</a></li>
               <li><a><div class = "ct-inline_item"><img src="images/home/line_icon.png" alt=""> </div> LINE: blackroa</a></li>
-              <li><div class = "ct-item"><a class="logo" target="_blank" href="https://line.me/ti/p/justkung1"><img src="images/home/addfriends_en.png" alt=""></a></div></li>
-              
+              <li><a target="_blank" href="https://line.me/ti/p/justkung1"><div class = "ct-inline_item"><img src="images/home/addfriends_en.png" alt=""> </div> </a></li>
         </div>
 
           </ul>
