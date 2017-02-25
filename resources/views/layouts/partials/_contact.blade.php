a0d73371d7d62b8d05d444e57f9174ac06a8e7ab
Strikermx <y.thanasit@gmail.com>
2017-02-25 06:34:06 -0800

    fix responsive contact bar

--- resources/views/layouts/partials/_contact.blade.php
+++ resources/views/layouts/partials/_contact.blade.php
@@ -15,7 +15,10 @@
         <div class="collapse navbar-collapse" id="ct-navbar-collapse">
           <ul class="nav navbar-nav navbar-right">
               <li>
-                <a><span class="glyphicon glyphicon-earphone"> 0909686520</span></a>
+                <a><span class="glyphicon glyphicon-envelope"> 0909686520</span></a>
+              </li>
+              <li>
+                <a><span class="glyphicon glyphicon-envelope"> y.thanasit@gmail.com</span></a>
               </li>
               <li>
                 <a target="_blank" href="https://line.me/ti/p/~justkung1"> LINE: blackroa <div class = "ct-inline_item"><img src="images/home/addfriends_en.png" alt=""></div></a> </li>
