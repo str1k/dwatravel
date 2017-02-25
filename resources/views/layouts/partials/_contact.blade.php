82bc8b29c999a7d69837dda0384ddc5049842273
Strikermx <y.thanasit@gmail.com>
2017-02-25 06:07:11 -0800

    fix responsive contact bar

--- resources/views/layouts/partials/_contact.blade.php
+++ resources/views/layouts/partials/_contact.blade.php
@@ -3,17 +3,37 @@
     <nav class="contact ct-navbar">
       <div class="container">
         <div class="navbar-header">
+        <button type="button" id="contact-bar" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#ct-navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+          </button>
               <a>ใบอนุญาติ 11/07318</a> 
         </div>
 
         <div class="collapse navbar-collapse" id="ct-navbar-collapse">
           <ul class="nav navbar-nav navbar-right">
-              <li><span class="glyphicon glyphicon-earphone"></span></li>
-              <li><a>0909686520</a></li>
-              <li><span class="glyphicon glyphicon-envelope"></span></li>
-              <li><a> y.thanasit@gmail.com</a></li>
-              <li><a><div class = "ct-inline_item"><img src="images/home/line_icon.png" alt=""> </div> LINE: blackroa</a></li>
-              <li><a target="_blank" href="https://line.me/ti/p/justkung1"><div class = "ct-inline_item"><img src="images/home/addfriends_en.png" alt=""> </div> </a></li>
+              <li>
+                <a>
+                  <span class="glyphicon glyphicon-earphone"></span>
+                   0909686520
+                </a>
+              </li>
+              <li>
+                <a>
+                  <span class="glyphicon glyphicon-envelope"></span>
+                   y.thanasit@gmail.com
+                </a>
+              </li>
+              <li>
+                <a target="_blank" href="https://line.me/ti/p/~justkung1"> 
+                  
+                   LINE: blackroa
+                  <div class = "ct-inline_item">
+                    <img src="images/home/addfriends_en.png" alt="">
+                  </div> 
+                </a> </li>
         </div>
 
           </ul>
@@ -21,4 +41,4 @@
       </div>
     </nav>
   </header>
-<!-- /CONTACT --> 
+<!-- /CONTACT -->   
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -33,7 +33,20 @@
         @include('layouts.partials._contact')
         @include('layouts.partials._header')
         
-    
+    <script type="text/javascript" src="js/jquery.min.js"></script><!-- jQuery -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script><!-- Bootstrap -->
+    <script type="text/javascript" src="js/jquery.parallax.js"></script><!-- Parallax -->
+    <script type="text/javascript" src="js/smoothscroll.js"></script><!-- Smooth Scroll -->
+    <script type="text/javascript" src="js/masonry.pkgd.min.js"></script><!-- masonry -->
+    <script type="text/javascript" src="js/jquery.fitvids.js"></script><!-- fitvids -->
+    <script type="text/javascript" src="js/owl.carousel.min.js"></script><!-- Owl-Carousel -->
+    <script type="text/javascript" src="js/jquery.counterup.min.js"></script><!-- CounterUp -->
+    <script type="text/javascript" src="js/waypoints.min.js"></script><!-- CounterUp -->
+    <script type="text/javascript" src="js/jquery.isotope.min.js"></script><!-- isotope -->
+    <script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script><!-- magnific-popup -->
+    <script type="text/javascript" src="js/scripts.js"></script><!-- Scripts -->
+
+
     
     </body>
 </html>
