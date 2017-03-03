d77028dd86a9141726da585ea8d24faa649197a8
Strikermx <y.thanasit@gmail.com>
2017-03-03 04:40:13 -0800

    test pic

--- resources/views/layouts/partials/_cover.blade.php
+++ resources/views/layouts/partials/_cover.blade.php
@@ -3,7 +3,8 @@
 	<section id="slider">
 		<div id="home-carousel" class="carousel slide" data-ride="carousel">			
 			<div class="carousel-inner">
-				<div class="item active" style="background-image: url(images/cover/cover_1.jpg)">
+				<div class="item active" >
+					<img src="images/cover/cover_1.jpg" alt="">
 					<div class="carousel-caption container">
 						<div class="row">
 							<div class="col-sm-7">
@@ -11,7 +12,8 @@
 						</div>
 					</div>					
 				</div>
-				<div class="item" style="background-image: url(images/cover/cover_2.jpg)">
+				<div class="item">
+					<img src="images/cover/cover_2.jpg" alt="">
 					<div class="carousel-caption container">
 						<div class="row">
 							<div class="col-sm-7">
@@ -19,7 +21,8 @@
 						</div>
 					</div>					
 				</div>
-				<div class="item" style="background-image: url(images/cover/cover_3.jpg)">
+				<div class="item" >
+					<img src="images/cover/cover_3.jpg" alt="">
 					<div class="carousel-caption container">
 						<div class="row">
 							<div class="col-sm-7">
@@ -30,6 +33,17 @@
 						</div>
 					</div>					
 				</div>
+				<!--<div class="item" style="background-image: url(images/cover/cover_3.jpg)">
+					<div class="carousel-caption container">
+						<div class="row">
+							<div class="col-sm-7">-->
+								<!--<h1>You are entire </h1>-->
+								<!--<h2>creative world</h2>-->
+								<!--<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor. Aenean sollicitudin, lorem quis bibendum auctor.</p>-->
+				<!--			</div>-->
+				<!--		</div>-->
+				<!--	</div>-->					
+				<!--</div>-->
 				<a class="home-carousel-left" href="#home-carousel" data-slide="prev"><i class="fa fa-angle-left"></i></a>
 				<a class="home-carousel-right" href="#home-carousel" data-slide="next"><i class="fa fa-angle-right"></i></a>
 			</div>		
