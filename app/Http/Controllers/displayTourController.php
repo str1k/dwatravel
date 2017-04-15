f1031f2d9ee25babe3f40b04b1922050b6bc4bc1
strikermx <y.thanasit@gmail.com>
2017-04-15 17:39:43 +0700

    add search by locate

--- app/Http/Controllers/displayTourController.php
+++ app/Http/Controllers/displayTourController.php
@@ -13,15 +13,23 @@ class displayTourController extends Controller
     public function filter(Request $request){
     $countries = country::all();
     $country = $request->input('country');
+    $locate_query = $request->input('locate_query');
     $now_str = \Carbon\Carbon::now()->format('d-m-Y');
     $clauses = [['id','>=','1']];
     $locate_clause =[];
-    $locate = [];
+    $locates = [];
     if (!empty($country)) {
     $in =['country','=',$country];
     array_push($locate_clause, $in);
     array_push($clauses, $in);
     $locates = locate::where($locate_clause)->get();
+    }
+    if (!empty($locate_query)) {
+      $in =['locate_list','LIKE','%'.$locate_query.'%'];
+      array_push($clauses, $in);
+      $in2 = ['locate','=',$locate_query];
+      array_push($locate_clause, $in2);
+      $locates = locate::where($locate_clause)->get(); 
     }
    $programs = programs::where($clauses)->whereDate('show_until', '>=', \Carbon\Carbon::now())->get();
    
--- resources/views/pages/tours.blade.php
+++ resources/views/pages/tours.blade.php
@@ -2,9 +2,18 @@
 @section('content')
 <div class="container-fluid" style="background-color: #edeaea;">
 	<div class="row">
-		<div class="col-lg-1">
+		<div class="col-lg-1 col-xs-1">
 		</div>	
-		<div class="col-lg-8">
+		<div class="col-lg-8 col-xs-8">
+			@foreach($locates as $locate)
+				<div class="row" style="margin-top: 20px; margin-bottom: 20px;">
+					<div class="col-lg-2 col-xs-3" >
+						<div class="flag-image2">
+						<a href="/program?query=&locate_query={{$locate->locate}}" ><img class="img-responsive" src="{{$locate->pic_url}}"></a>
+						</div>
+					</div>
+				</div>
+			@endforeach
 			@foreach($programs as $program)
 			<div class="row" style="border: 1px solid black; background-color:white; margin-bottom: 20px; ">
 			 	<div class="col-lg-12">
@@ -13,7 +22,7 @@
 		padding-top: 2%;padding-bottom: 2%;">
 						<h3 style="background-color :#edeaea; color:red;">{{$program->name}}</h3>
 						<div class="row">
-							<div class="col-lg-10">
+							<div class="col-lg-10 col-xs-10">
 								<a><img class="img-responsive" src="{{$program->tour_pic}}" alt=""></a>
 							</div>
 						</div>
@@ -21,10 +30,10 @@
 						<?php echo $program->content ?>
 						<br></br>
 						<div class="row">
-							<div class="col-lg-3">
+							<div class="col-lg-3 col-xs-3">
 							<a href="detail?query=&program_id={{$program->id}}#book" target="_blank" class="btn btn-success">จองทัวร์คลิก</a>
 							</div>
-							<div class="col-lg-3">
+							<div class="col-lg-3 col-xs-3">
 							<a href="/detail?query=&program_id={{$program->id}}#description" target="_blank" class="btn btn-primary">ดูรายละเอียดโปรแกรมทัวร์</a>
 							</div>
 						</div>
@@ -34,7 +43,7 @@
 			</div>
 			@endforeach
 		</div>
-		<div class="col-lg-3">
+		<div class="col-lg-3 col-xs-3">
 			<div class="row">
 				<div class="col-lg-12" >
 
