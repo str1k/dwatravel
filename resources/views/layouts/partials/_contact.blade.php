368b682768eb6db4f49c38c28a6a30d2222401be
strikermx <y.thanasit@gmail.com>
2017-04-15 23:13:16 +0700

    add follow up file

--- resources/views/layouts/partials/_contact.blade.php
+++ resources/views/layouts/partials/_contact.blade.php
@@ -15,7 +15,7 @@
             <span class="sr-only">Toggle navigation</span>
               
           </button>
-              <span style="font-family: arial, helvetica, sans-serif;"><a style="margin-left: 10px;"></a>{{$contact_bar->autorize}} </span>
+              <span ><a style="margin-left: 10px;"></a>{{$contact_bar->autorize}} </span>
         </div>
 
         <div class="collapse navbar-collapse collapse show" id="ct-navbar-collapse">
