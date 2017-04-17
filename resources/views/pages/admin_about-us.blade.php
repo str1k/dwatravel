5b6fe904173d1836f028ae3f8622108306ee1ddc
strikermx <y.thanasit@gmail.com>
2017-04-17 12:53:49 +0700

    fix menu and country admin

--- resources/views/pages/admin_about-us.blade.php
+++ resources/views/pages/admin_about-us.blade.php
@@ -1,5 +1,32 @@
 @extends('admin_master')
 @section('content')
+<script>tinymce.init({
+  selector: 'textarea',
+  height: 300,
+  theme: 'modern',
+  plugins: [
+    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+    'searchreplace wordcount visualblocks visualchars code fullscreen',
+    'insertdatetime media nonbreaking save table contextmenu directionality',
+    'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc responsivefilemanager'
+  ],
+  toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link',
+  toolbar2: 'responsivefilemanager | print preview media | forecolor backcolor emoticons | sizeselect | fontselect |  fontsizeselect' ,
+  toolbar3: '',
+  external_filemanager_path:"/pic_upload/",
+   filemanager_title:"Responsive Filemanager" ,
+   external_plugins: { "filemanager" : "plugins/responsivefilemanager/plugin.min.js"},
+  image_advtab: true,
+  templates: [
+    { title: 'Test template 1', content: 'Test 1' },
+    { title: 'Test template 2', content: 'Test 2' }
+  ],
+  content_css: [
+    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+    '//fonts.googleapis.com/css?family=Kanit',
+    '//www.tinymce.com/css/codepen.min.css'
+  ]
+ });</script>
 <div id="page-wrapper">
 	<div class="container-fluid">
 <!-- Page Heading -->
